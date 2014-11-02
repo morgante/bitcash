717b1d1591f860fabaddb4dbbab16ba8b8417389
Morgante Pell <morgante.pell@morgante.net>
2014-11-01 17:23:38 -0700

    start playing with firebase

--- public/receive.js
+++ public/receive.js
@@ -3,9 +3,47 @@
 
 	var firebase = new Firebase("https://blinding-heat-4116.firebaseio.com/");
 
+	var users = firebase.child("users");
+	var email = 'me@morgante';
+	var userRef = users.child(btoa(email));
+	var user;
+
+	var tid;
+
+	var $form;
+	var $address;
+	var $wait;
+
+	function wait() {
+		$wait = $('.waiting');
+
+		$form.slideUp();
+		$wait.slideDown();
+
+		debugger;
+		console.log(user);
+	}
+
 	function init() {
-		getBalance('1Pxf8H2V9vjev7PpWixgqZqtCgtrwqtKeX', function(a, b) {
-			console.log('a', a, b);
+		tid = $('[data-transaction-id]').data('transaction-id');
+
+		userRef.on('value', function(snapshot) {
+			user = snapshot.val();
+		});
+
+		$form = $('form');
+		$address = $('#address');
+
+		$form.submit(function(evt) {
+			evt.preventDefault();
+
+			var address = $address.val();
+
+			userRef.set({
+				address: address
+			});
+
+			wait();
 		});
 	}
 
